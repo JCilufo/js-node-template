dc3d83505847bb3ede0bbfbdc09258d5e661ab25
JCilufo <joshua.cilufo@student.swic.edu>
2025-02-23 23:04:51 -0600

    Add string formatting code

---
+++ src/strings.js
@@ -0,0 +1,12 @@
+const name = "Joshua Cilufo";
+
+const course = "Web Development 1";
+
+const grade = 100;
+
+const tradString =
+	name + ` is taking ` + course + ` and expects to earn ` + grade + `%.`;
+
+const tempLiteral = `${name} is taking ${course} and expects to earn ${grade}%.`;
+
+console.log(tradString, tempLiteral);
