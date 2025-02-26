4216c079179b2edd66ae03ff90223c6b183c525d
Joshua Cilufo <joshua.cilufo@student.swic.edu>
2025-02-25 18:45:41 -0600

    Calculate order total

--- src/functions.js
+++ src/functions.js
@@ -7,13 +7,3 @@ function calculateTotal(order) {
 	return 10 * 2;
 }
 console.log(calculateTotal(order));
-
-// Test 2: Format a username
-const username = {
-	first: "John",
-	last: "Doe",
-};
-function formatUsername(formatUsername) {
-	return `${username.last}, ${username.first}`;
-}
-console.log(formatUsername("John", "Doe"));
