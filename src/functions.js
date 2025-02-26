354df18e3352d6f16339accc76ffbfb2ae1970ce
Joshua Cilufo <joshua.cilufo@student.swic.edu>
2025-02-25 19:06:06 -0600

    Format a username

--- src/functions.js
+++ src/functions.js
@@ -7,3 +7,13 @@ function calculateTotal(order) {
 	return 10 * 2;
 }
 console.log(calculateTotal(order));
+
+// Test 2: Format a username
+const username = {
+	first: "John",
+	last: "Doe",
+};
+function formatUsername(formatUsername) {
+	return `${username.last}, ${username.first}`;
+}
+console.log(formatUsername("John", "Doe"));
